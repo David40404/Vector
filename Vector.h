49b40eb86768e152d5588b23c56a3493a9319d60
David40404 <88509902+David40404@users.noreply.github.com>
2024-05-09 22:39:45 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -3,11 +3,12 @@
 
 namespace Vector {
 	template <typename T>
-	class Vector2 {
+  	class Vector2 {
 	public:
 		T x, y;
 
-		Vector2(T x = 0, T y = 0) : x(x), y(y) {}
+		Vector2() {}
+		Vector2(T x, T y) : x(x), y(y) {}
 
 		Vector2 operator+(const Vector2& other) { return Vector2(x + other.x, y + other.y); }
 		Vector2 operator-(const Vector2& other) { return Vector2(x - other.x, y - other.y); }
@@ -28,28 +29,44 @@ namespace Vector {
 		return os << other.x << " " << other.y;
 	}
 
-  template <typename T>
-  class Vector3 {
-  public:
-  	T x, y, z;
+  	template <typename T>
+  	class Vector3 {
+  	public:
+  		T x, y, z;
   
-  	Vector3(T x = 0, T y = 0, T z = 0) : x(x), y(y), z(z) {}
+		Vector3() {}
+		Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
   
-  	Vector3 operator+(const Vector3& other) { return Vector3(x + other.x, y + other.y, z + other.z); }
-  	Vector3 operator-(const Vector3& other) { return Vector3(x - other.x, y - other.y, z - other.z); }
-  	Vector3 operator*(const Vector3& other) { return Vector3(x * other.x, y * other.y, z * other.z); }
-  	Vector3 operator/(const Vector3& other) { return Vector3(x / other.x, y / other.y, z / other.z); }
+  		Vector3 operator+(const Vector3& other) { return Vector3(x + other.x, y + other.y, z + other.z); }
+  		Vector3 operator-(const Vector3& other) { return Vector3(x - other.x, y - other.y, z - other.z); }
+  		Vector3 operator*(const Vector3& other) { return Vector3(x * other.x, y * other.y, z * other.z); }
+  		Vector3 operator/(const Vector3& other) { return Vector3(x / other.x, y / other.y, z / other.z); }
   
-  	bool operator==(const Vector3& other) { return x == other.x and y == other.y and z == other.z; }
+  		bool operator==(const Vector3& other) { return x == other.x and y == other.y and z == other.z; }
   
-  	Vector3& operator+=(const Vector3& other) { x += other.x; y += other.y; z += other.z; return *this; }
-  	Vector3& operator-=(const Vector3& other) { x -= other.x; y -= other.y; z -= other.z; return *this; }
-  	Vector3& operator*=(const Vector3& other) { x *= other.x; y *= other.y; z *= other.z; return *this; }
-  	Vector3& operator/=(const Vector3& other) { x /= other.x; y /= other.y; z /= other.z; return *this; }
-  };
+  		Vector3& operator+=(const Vector3& other) { x += other.x; y += other.y; z += other.z; return *this; }
+  		Vector3& operator-=(const Vector3& other) { x -= other.x; y -= other.y; z -= other.z; return *this; }
+  		Vector3& operator*=(const Vector3& other) { x *= other.x; y *= other.y; z *= other.z; return *this; }
+  		Vector3& operator/=(const Vector3& other) { x /= other.x; y /= other.y; z /= other.z; return *this; }
+ 	 };
   
-  template <typename T>
-  std::ostream& operator<<(std::ostream& os, const Vector3 <T>& other) {
-  	return os << other.x << " " << other.y << " " << other.z;
-  }
+ 	 template <typename T>
+ 	 std::ostream& operator<<(std::ostream& os, const Vector3 <T>& other) {
+  		return os << other.x << " " << other.y << " " << other.z;
+ 	 }
+
+
+	typedef Vector2 <int>      Vector2i;
+	typedef Vector2 <unsigned> Vector2u;
+	typedef Vector2 <float>    Vector2f;
+	typedef Vector2 <double>   Vector2d;
+	typedef Vector2 <long long> Vector2lli;
+	typedef Vector2 <unsigned long long> Vector2llu;
+
+	typedef Vector3 <int>      Vector3i;
+	typedef Vector3 <unsigned> Vector3u;
+	typedef Vector3 <float>    Vector3f;
+	typedef Vector3 <double>   Vector3d;
+	typedef Vector3 <long long> Vector3lli;
+	typedef Vector3 <unsigned long long> Vector3llu;
 }
